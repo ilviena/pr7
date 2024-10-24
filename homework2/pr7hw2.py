fac2244785f4edffd77dbdaae09da4bf719a7861
Ильвина Ямалетдинова <yamaletdinova.ilvina@gmail.com>
2024-10-24 19:16:13 +0500

    Добавила возможность вводить отрицательные целые числа

--- homework2/pr7hw2.py
+++ homework2/pr7hw2.py
@@ -1,5 +1,10 @@
-x = input('Введите целое неотрицательное число: ')
-if x.isdigit():
+x = input('Введите целое число: ')
+if x[0]=='-' and x[1:].isdigit():
+    x_bin = '-' + bin(int(x))[3:]
+    x_oct = '-' + oct(int(x))[3:]
+    print(f"Число {x} в 2-ой сс - {x_bin}")
+    print(f"Число {x} в 8-ой сс - {x_oct}")
+elif x.isdigit():
     x_bin = bin(int(x))[2:]
     x_oct = oct(int(x))[2:]
     print(f"Число {x} в 2-ой сс - {x_bin}")
